dbc37db1163c7bfcd8130141b39f03fb2d751e2f
Gregory Desfour <gregory.desfour@accedo.tv>
2016-11-25 14:55:56 +0100

    Minor shell change

--- shellCmds.sh
+++ shellCmds.sh
@@ -1,4 +1,6 @@
 # no delay before showing the dock on autohide
-defaults write com.apple.Dock autohide-delay -float 0 && killall Dock
+defaults write com.apple.Dock autohide-delay -float 0
 # no animation either
-defaults write com.apple.dock autohide-time-modifier -int 0 && killall Dock
+defaults write com.apple.dock autohide-time-modifier -int 0
+# restart the dock
+killall Dock
